e491e7db77d178078449c6c4dd3c14d8b63e229c
MinhHieuDL <minhhieuhcmute@gmail.com>
2025-07-19 01:16:17 +0700

    complete the raw version of fwStateMachine

--- codePractice/stateMachine/include/fwStateMachine.h
+++ codePractice/stateMachine/include/fwStateMachine.h
@@ -1,9 +1,29 @@
-class fwStateMachine
-{
-private:
-    /* data */
-public:
-    fwStateMachine(/* args */);
-    ~fwStateMachine();
+typedef struct{
+    unsigned char m_eventID;
+    unsigned char m_nextState;
+    unsigned char m_actionID;
+} STATE_EVENT_CONTROL;
+
+typedef struct{
+    unsigned char m_stateID;
+    unsigned char m_enterActionID;
+    unsigned char m_exitActionID;
+    STATE_EVENT_CONTROL* m_pEventControlStateList;
+} STATE;
+
+class CStateMachine {
+    private:
+        STATE m_CurrentState;
+        STATE* m_pStateList;
+        
+        void LoadStateFromStateID(unsigned char ucState, STATE& refState);
+    protected:
+        virtual void CallMapAction(unsigned char ucActionID) = 0;
+    
+    public:
+        void Init(STATE* pStateList, unsigned char ucFirstState);
+        void HandleEvt(unsigned char ucEvt);
+        CStateMachine(/* args */);
+        ~CStateMachine();
 };
 
--- codePractice/stateMachine/src/fwStateMachine.cpp
+++ codePractice/stateMachine/src/fwStateMachine.cpp
@@ -3,12 +3,64 @@
 
 using namespace std;
 
-fwStateMachine::fwStateMachine(/* args */)
-{
-    cout << "State Machine created" << endl;
-}
+CStateMachine::CStateMachine(/* args */) : 
+    m_CurrentState{0},
+    m_pStateList{nullptr}
+{ };
 
-fwStateMachine::~fwStateMachine()
+CStateMachine::~CStateMachine()
 {
     cout << "State Machine removed" << endl;
+}
+
+
+void CStateMachine::Init(STATE* pStateList, unsigned char ucFirstState){
+    // load the state list
+    m_pStateList = pStateList;
+    
+    //load the first state
+    LoadStateFromStateID(ucFirstState, m_CurrentState);
+
+    // Perform the enter init state action
+    CallMapAction(m_CurrentState.m_enterActionID);
+}
+
+void CStateMachine::LoadStateFromStateID(unsigned char ucState, STATE& refState){
+    unsigned char ucSize = sizeof(m_pStateList)/sizeof(STATE);
+    for(unsigned i = 0; i < ucSize; i++){
+        if(m_pStateList[i].m_stateID == ucState){
+            refState = m_pStateList[i];
+            break;
+        }
+    }
+    cout << "Failed to load the State from state ID" << endl;
+}
+
+void CStateMachine::HandleEvt(unsigned char ucEvt) {
+    STATE_EVENT_CONTROL* pListEvtControl = m_CurrentState.m_pEventControlStateList;
+    unsigned char ucSize = sizeof(pListEvtControl)/sizeof(STATE_EVENT_CONTROL);
+    for(unsigned i = 0; i < ucSize; i++)
+    {
+        bool b_IsDone{false};
+        if(pListEvtControl[i].m_eventID == ucEvt)
+        {
+            CallMapAction(pListEvtControl[i].m_actionID);
+            b_IsDone = true;
+        }
+
+        // check if need to switch state
+        if(pListEvtControl[i].m_nextState != m_CurrentState.m_stateID)
+        {
+            // exit current state
+            CallMapAction(m_CurrentState.m_exitActionID);
+            
+            // Load new state
+            LoadStateFromStateID(pListEvtControl[i].m_nextState, m_CurrentState);
+            
+            // do enter new state action
+            CallMapAction(m_CurrentState.m_enterActionID);
+        }
+        
+        if(b_IsDone) break;
+    }
 }
