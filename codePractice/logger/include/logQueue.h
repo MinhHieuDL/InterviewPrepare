b13defa96b06594f8ef6432f86582bd920c51a77
MinhHieuDL <minhhieuhcmute@gmail.com>
2026-01-04 21:32:27 +0700

    remove sink logger from logQueue - follow single responsibility principle

--- codePractice/logger/include/logQueue.h
+++ codePractice/logger/include/logQueue.h
@@ -3,18 +3,16 @@
 #include <queue>
 #include <string>
 #include <mutex>
-#include "iLog.h"
 
 class logQueue{
     private:
         std::queue<std::string> m_logQueue;
         std::mutex m_QueueMutex;
-        iLog* m_pLog;
+
 
     public:
-        logQueue(iLog* pLogConcrete);
+        logQueue()=default;
         ~logQueue()=default;
 
         void pushMsg(const std::string logMsg);
-        void writeLog();
 };
--- codePractice/logger/src/logQueue.cpp
+++ codePractice/logger/src/logQueue.cpp
@@ -1,21 +1,9 @@
 #include <thread>
 #include "logQueue.h"
-
-logQueue::logQueue(iLog* pLogConcrete) : m_pLog(pLogConcrete)
-{}
         
 void logQueue::pushMsg(const std::string logMsg)
 {
     std::unique_lock<std::mutex> lock(m_QueueMutex);
     m_logQueue.push(logMsg);
     lock.unlock();
-}
-
-void logQueue::writeLog()
-{
-    while(!m_logQueue.empty())
-    {
-        m_pLog->log(LOG_LVL::INFO, m_logQueue.front());
-        m_logQueue.pop();
-    }
 }
