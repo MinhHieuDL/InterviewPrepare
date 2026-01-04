9f6a736b7cce0f5e3cd5181655b36880b4630487
MinhHieuDL <minhhieuhcmute@gmail.com>
2026-01-04 22:39:59 +0700

    add stop mechanics to safely exit the waiting loop from caller

--- codePractice/logger/include/logQueue.h
+++ codePractice/logger/include/logQueue.h
@@ -5,16 +5,19 @@
 #include <mutex>
 #include <condition_variable>
 
+
 class logQueue{
     private:
         std::queue<std::string> m_logQueue;
         std::mutex m_QueueMutex;
         std::condition_variable m_cv;
+        bool m_bStopped{false};
 
     public:
         logQueue()=default;
         ~logQueue()=default;
 
-        void pushMsg(const std::string logMsg);
-        void wait_and_pop(std::string& msg);
+        bool pushMsg(std::string logMsg);
+        bool wait_and_pop(std::string& msg);
+        void shutdown();
 };
--- codePractice/logger/src/logQueue.cpp
+++ codePractice/logger/src/logQueue.cpp
@@ -1,19 +1,47 @@
 #include "logQueue.h"
-        
-void logQueue::pushMsg(const std::string logMsg)
+
+void logQueue::shutdown()
 {
     std::unique_lock<std::mutex> lock(m_QueueMutex);
-    m_logQueue.push(logMsg);
+    m_bStopped = true;
     lock.unlock();
-    m_cv.notify_one();
+    m_cv.notify_all();
 }
 
-void logQueue::wait_and_pop(std::string& msg)
+bool logQueue::pushMsg(std::string logMsg)
+{
+    bool bPushed = false;
+    
+    {
+        std::unique_lock<std::mutex> lock(m_QueueMutex);
+        if(!m_bStopped)
+        {
+            m_logQueue.push(std::move(logMsg));
+            bPushed = true;
+        }
+    }
+    
+    if(bPushed)
+    {
+        m_cv.notify_one();
+    }
+
+    return bPushed;
+}
+
+bool logQueue::wait_and_pop(std::string& msg)
 {
     std::unique_lock<std::mutex> lock(m_QueueMutex);
-    while(m_logQueue.empty())
-        m_cv.wait(lock);
-    msg = m_logQueue.front();
+    
+    // Wait until there is data to comsume or shutdown requested
+    m_cv.wait(lock, [this] {
+        return !m_logQueue.empty() || m_bStopped;
+    });
+    
+    // if producer shutdown and no more messages exits, signal caller to exit
+    if(m_logQueue.empty()) return false;
+
+    msg = std::move(m_logQueue.front());
     m_logQueue.pop();
-    lock.unlock();
+    return true;
 }
