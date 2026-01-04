2957f4f00286296b3d43d9acdf78c73a4ffc6ab8
MinhHieuDL <minhhieuhcmute@gmail.com>
2026-01-04 21:46:39 +0700

    using condition variable to sync the producer and consumer

--- codePractice/logger/include/logQueue.h
+++ codePractice/logger/include/logQueue.h
@@ -3,16 +3,18 @@
 #include <queue>
 #include <string>
 #include <mutex>
+#include <condition_variable>
 
 class logQueue{
     private:
         std::queue<std::string> m_logQueue;
         std::mutex m_QueueMutex;
-
+        std::condition_variable m_cv;
 
     public:
         logQueue()=default;
         ~logQueue()=default;
 
         void pushMsg(const std::string logMsg);
+        void wait_and_pop(std::string& msg);
 };
--- codePractice/logger/src/logQueue.cpp
+++ codePractice/logger/src/logQueue.cpp
@@ -1,9 +1,19 @@
-#include <thread>
 #include "logQueue.h"
         
 void logQueue::pushMsg(const std::string logMsg)
 {
     std::unique_lock<std::mutex> lock(m_QueueMutex);
     m_logQueue.push(logMsg);
+    lock.unlock();
+    m_cv.notify_one();
+}
+
+void logQueue::wait_and_pop(std::string& msg)
+{
+    std::unique_lock<std::mutex> lock(m_QueueMutex);
+    while(m_logQueue.empty())
+        m_cv.wait(lock);
+    msg = m_logQueue.front();
+    m_logQueue.pop();
     lock.unlock();
 }
